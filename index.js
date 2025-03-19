1a9e52db18ae1fbd93bb3cc44431f2f2080c888b
AnantaBG <anantabanikofficial@gmail.com>
2025-03-19 22:58:13 +0600

    All Events Data Fetch

--- index.js
+++ index.js
@@ -25,83 +25,8 @@ async function run() {
     const db = client.db('CommunionDB');
     const allEvents = db.collection('EventCL');
 
-    // app.patch('/alltasks/:id', async (req, res) => {
-    // const taskId = req.params.id;
-    //   const updateData = req.body;
-  
-  
-    //   try {
-    //       const objectId = new ObjectId(taskId);
-  
-    //       const existingTask = await allTasks.findOne({ _id: objectId });
-  
-    //       if (!existingTask) {
-    //           return res.status(404).json({ message: "Task not found" });
-    //       }
-  
-  
-    //       let updateSet = {};
-  
-    //       if (updateData.order !== undefined) {
-    //           const order = parseInt(updateData.order);
-    //           updateSet.order = order;
-    //       }
-  
-    //       if (updateData.category) {
-    //           updateSet.category = updateData.category;
-    //       }
-  
-    //       // Add other fields to updateSet ONLY IF they are present in updateData
-    //       for (const key in updateData) {
-    //           if (key !== 'order' && key !== 'category') { // Exclude order and category
-    //               updateSet[key] = updateData[key];
-    //           }
-    //       }
-  
-  
-  
-    //       if (Object.keys(updateSet).length === 0) { // Check if updateSet is empty
-    //           return res.status(400).json({ message: "No fields to update provided" });
-    //       }
-  
-    //       const updateResult = await allTasks.updateOne({ _id: objectId }, { $set: updateSet });
-  
-  
-    //       if (updateResult.modifiedCount === 0) {
-    //           return res.status(404).json({ message: "Task not found or not updated" });
-    //       }
-  
-    //       return res.status(200).json({ message: "Task updated successfully" });
-    //   } catch (error) {
-    //       console.error("Error updating task:", error);
-    //       res.status(500).json({ error: 'Failed to update task' });
-    //   }
-    // });
 
-    // app.get('/allTasks', async (req, res) => {
-    // try {
-    //     let query = {};
 
-    //     if (req.query.category) {
-    //         const category = req.query.category;
-    //         const allowedCategories = ["To-Do", "In Progress", "Done"];
-    //         if (allowedCategories.includes(category)) {
-    //             query = { category: category };
-    //         } else {
-    //             return res.status(400).send("Invalid category");
-    //         }
-    //     }
-
-    //     // *** Corrected line - Add the .sort({ order: 1 }) ***
-    //     const result = await allTasks.find(query).sort({ order: 1 }).toArray();
-
-    //     res.send(result);
-
-    // } catch (error) {
-    //     console.error("Error in /allTasks route:", error);
-    //     res.status(500).send("Internal server error");
-    // }
-    // });
 
     // app.post('/users', async (req, res) => {
     //   const user = req.body;
@@ -153,10 +78,10 @@ async function run() {
     //   }
     // });
 
-    // app.get('/allUsers', async (req, res) => {
-    //   const result = await allUsers.find().toArray();
-    //   res.send(result);
-    // });
+    app.get('/allEvents', async (req, res) => {
+      const result = await allEvents.find().toArray();
+      res.send(result);
+    });
 
 
 
